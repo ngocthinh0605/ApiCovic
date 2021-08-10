f408063ca5b937ee31e34755947bae4492a01917
Ngoc Thinh <ngocthinh0605@gmail.com>
2021-08-10 16:24:24 +0700

    fix loading

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import { useState, useEffect } from 'react'
 import Covid from './components/Covid';
 import SearchBar from './components/SearchBar';
-import InfiniteScroll from 'react-infinite-scroll-component';
 import './app.css'
 import './Grid.css'
 
 function App() {
+  
 
   const [apiCovid, setApiCovid] = useState([]);
   const [filterText, setFilterText] = useState('');
   
- 
    useEffect(() =>{
     const getApiCovid = async () =>{
       const takeApiCovid = await fetchApiCovid();
@@ -19,39 +18,37 @@ function App() {
     getApiCovid();
   },[]);
 
+
   const fetchApiCovid = async () =>{
     const res = await fetch('https://api.covid19api.com/summary');
     const dataApi = await res.json();
     return dataApi;
   }
 
-
-  
-
-
   function handleFilterTextChange(filterText){
     console.log(filterText);
     setFilterText(filterText);
   }
+
+
   return (
     <div className="App">  
+              
                     <h1>Covid Trên Thế Giới</h1>
                     <SearchBar 
                       filterText = {filterText}
                       onFilterTextChange={handleFilterTextChange}
                     />
-                  {/* <InfiniteScroll 
-                    dataLength={apiCovid.length} //This is important field to render the next data
-                    next={fetchData}
-                    hasMore={true}> */}
+                    
                     {
-                      apiCovid.length === 0 ? <div className="loading">
-                        <h1>loading...</h1>
+                      apiCovid.length === 0 ? 
+                      <div className='loading'>
+                        <h1>Loading...</h1>
                       </div> : <Covid datas = {apiCovid}  
                                       filterText={filterText}
                                       />
                     }
-                  {/* </InfiniteScroll> */}
+              
             </div>
   );
 }
--- src/components/Covid.js
+++ src/components/Covid.js
@@ -1,15 +1,41 @@
 import Country from './Country'
+import InfiniteScroll from 'react-infinite-scroll-component';
+import { useState } from 'react';
+
 const Covid = ({ datas, filterText }) => {
     datas.forEach(data => {
         if(data["Country"].indexOf(filterText) > -1){
         }
     })
 
+    const [count, setCount] = useState({
+        prev: 0,
+        next: 10
+      })
+      const [hasMore, setHasMore] = useState(true);
+      const [current, setCurrent] = useState(datas.slice(count.prev, count.next))
+      const getMoreData = () => {
+        if (current.length === datas.length) {
+          setHasMore(false);
+          return;
+        }
+        setTimeout(() => {
+          setCurrent(current.concat(datas.slice(count.prev + 10, count.next + 10)))
+        }, 2000)
+        setCount((prevState) => ({ prev: prevState.prev + 10, next: prevState.next + 10 }))
+      }
+
     return (
         <>
+            <InfiniteScroll
+            dataLength={current.length}
+            next={getMoreData}
+            hasMore={hasMore}
+            loader={<h4>Loading...</h4>}
+            >
             {
-                datas
-                .map((data) => {
+                // eslint-disable-next-line
+                current.map((data) => {
                     if(filterText === ''){
                             return <Country key={data.ID} country={data}></Country>
                         }else if(data["Country"].indexOf(filterText) > -1){
@@ -17,6 +43,7 @@ const Covid = ({ datas, filterText }) => {
                         }
                 })
             }
+            </InfiniteScroll>
         </>
     )
 }
